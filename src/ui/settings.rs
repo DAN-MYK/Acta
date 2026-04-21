ae58bbd5683dbe64a2a100a5460928d85ad09efd
DAN-MYK <dunmisha@gmail.com>
2026-04-21 18:47:55 +0300

    feat(ui): add settings stub

--- src/ui/settings.rs
+++ src/ui/settings.rs
@@ -1,1 +1,14 @@
-// TODO: налаштування програми
+pub fn apply_settings_to_ui(ui: &crate::AppWindow) {
+    ui.set_company_info(crate::CompanyInfo {
+        full_name: slint::SharedString::default(),
+        short_name: slint::SharedString::default(),
+        edrpou: slint::SharedString::default(),
+        ipn: slint::SharedString::default(),
+        address: slint::SharedString::default(),
+        director: slint::SharedString::default(),
+        iban: slint::SharedString::default(),
+        bank: slint::SharedString::default(),
+        vat_registered: false,
+        vat_cert: slint::SharedString::default(),
+    });
+}
